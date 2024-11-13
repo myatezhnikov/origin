ea9eaf1ca966894f4130f3e94324127321770f9f
myatezhnikov <myatezhnikov2014@gmail.com>
2024-11-14 00:49:04 +0400

    new file task 3.2

---
+++ lesson_3/Task_2/ConsoleApplication1/ConsoleApplication1/Task_2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+class Counter
+{
+private:
+    int i=1;
+
+public:
+    Counter (int i) {}
+    Counter() { i = 1; }
+    void count_Up ()
+    {
+        i = i++;
+    }
+    void count_Down ()
+    {
+        i = i--;
+    }
+    void print ()
+    {
+        std::cout << i << std::endl;
+    }
+    
+};
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    std::string s;
+   // Counter count;
+    int x=1;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> s; std::cout << "\n" << s;
+    if (s == "да") 
+    { 
+        std::cout << "Введите начальное значение счётчика: "; 
+        std::cin >> x; 
+       // Counter count(x);
+    }
+    else if (s == "нет") {
+        x = 1; 
+       // Counter count;
+    }
+    Counter count(x);
+    char command;
+    do
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command; std::cout << command << std::endl;
+        switch (command)
+        {
+        case 43: count.print();  break;
+        case 45: break;
+        case 61: count.print(); break;
+        }
+    } while (command == 120);
+         
+}
