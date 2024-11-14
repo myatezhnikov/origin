e55b49ba12bc41d6e81f42bf79283f7d3bbb3eaf
myatezhnikov <myatezhnikov2014@gmail.com>
2024-11-14 19:07:40 +0400

    task 3.2.1

--- lesson_3/Task_2/ConsoleApplication1/ConsoleApplication1/Task_2.cpp
+++ lesson_3/Task_2/ConsoleApplication1/ConsoleApplication1/Task_2.cpp
@@ -1,56 +1,51 @@
 ﻿#include <iostream>
+#include <windows.h>
 class Counter
 {
 private:
     int i=1;
 
 public:
-    Counter (int i) {}
+    Counter(int i) { this->i = i; }
     Counter() { i = 1; }
     void count_Up ()
     {
-        i = i++;
+        this->i = ++this->i;
     }
     void count_Down ()
     {
-        i = i--;
+        this->i = --this->i;
     }
     void print ()
     {
-        std::cout << i << std::endl;
-    }
-    
+        std::cout << this->i << std::endl;
+    }    
 };
 int main()
 {   
     setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
     std::string s;
-   // Counter count;
     int x=1;
     std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-    std::cin >> s; std::cout << "\n" << s;
+    std::cin >> s;
     if (s == "да") 
     { 
         std::cout << "Введите начальное значение счётчика: "; 
-        std::cin >> x; 
-       // Counter count(x);
-    }
-    else if (s == "нет") {
-        x = 1; 
-       // Counter count;
-    }
+        std::cin >> x;       
+    }   
     Counter count(x);
     char command;
     do
     {
         std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-        std::cin >> command; std::cout << command << std::endl;
+        std::cin >> command; 
         switch (command)
         {
-        case 43: count.print();  break;
-        case 45: break;
+        case 43: count.count_Up(); break;
+        case 45: count.count_Down(); break;
         case 61: count.print(); break;
         }
-    } while (command == 120);
-         
+    } while (command != 120);
+    std::cout << "До свидания!" << std::endl;
 }
