c8cd6fba213189eb60753683af840421c34fb274
myatezhnikov <myatezhnikov2014@gmail.com>
2025-03-05 23:25:36 +0400

    cppl 8

---
+++ cppl/ConsoleApplication_8.1/ConsoleApplication_8.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+template <typename T>
+void print(std::vector<T> vec)      //функция вывода вектора на экран
+{
+    for (int i : vec)
+    {
+        std::cout << i << " ";
+    }
+}
+
+int main()
+{
+    std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";   
+    print(vec);                                             //выводим на экран
+    std::sort(vec.begin(), vec.end());                      //сортируем
+    auto it = std::unique(vec.begin(), vec.end());          //отбираем уникальные
+    vec.erase(it, vec.end());                               //удаляем лишние
+    std::cout << std::endl << "[OUT]: ";
+    print(vec);                                             //выводим на экран
+    std::cout << std::endl;
+    system("Pause");
+    return 0;
+}
---
+++ cppl/ConsoleApplication_8.2/ConsoleApplication_8.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <list>
+
+template <typename T>
+void print_container(T data)
+{	
+	for (auto it = data.begin(); it != data.end(); it++)    //выводим на экран через итератор
+	{
+		std::cout << *it << ", ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+
+	std::cout << std::endl;
+	system("Pause");
+	return 0;
+};
