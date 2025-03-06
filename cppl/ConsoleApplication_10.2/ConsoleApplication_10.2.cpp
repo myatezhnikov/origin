33befc268b083a064a9d5811805dac7fc8d243de
myatezhnikov <myatezhnikov2014@gmail.com>
2025-03-06 20:27:50 +0400

    Update ConsoleApplication_10.2.cpp

--- cppl/ConsoleApplication_10.2/ConsoleApplication_10.2.cpp
+++ cppl/ConsoleApplication_10.2/ConsoleApplication_10.2.cpp
@@ -6,7 +6,7 @@ class node
 {
 public:
     int m_value;
-    std::weak_ptr<node> parent;
+    std::weak_ptr<node> parent;   //изменил на weak_ptr
     node(int value) : m_value{ value } {};
     ~node() { std::cout << "destructor called\n"; }
 };
