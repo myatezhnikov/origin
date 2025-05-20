ab7a6b7e0975413ceb4747099d927ec6974fee6a
myatezhnikov <myatezhnikov2014@gmail.com>
2025-05-20 21:50:30 +0400

    MAP_1.2

--- MAP/1/ConsoleApplication2/ConsoleApplication2.cpp
+++ MAP/1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include <mutex>
 using namespace std;
 
 vector<double> intialize(vector<double>& vec, size_t& size)	//Инициализация векторов целыми числами, последовательно
@@ -10,73 +11,68 @@ vector<double> intialize(vector<double>& vec, size_t& size)	//Инициализ
 	{
 		vec.push_back(i);
 	}
-	cout << vec.size() << "\t";
+	cout << vec.size() << "\t\t";
 	return vec;
 }
 
 
-vector<double> sumVector(vector<double>& vec)		//сумма двух одинаковых векторов
+void sumVector(const vector<double>& vec1, const vector<double>& vec2, vector<double>& vec3, size_t start, size_t end)		//сумма двух одинаковых векторов
 {
-	vector<double> sum;
-	for (size_t i = 0; i < vec.size(); i++)
+	for (size_t i = start; i < end; i++)
 	{
-		sum.push_back(vec[i]+vec[i]);		
+		vec3[i] = vec1[i] + vec2[i];
 	}
-	return sum;
 }
-void timeLaps(vector<double>& vec)					//подсчет времени выполнения суммирования
-{
-	auto start = chrono::steady_clock::now();
-	sumVector(vec);
-	auto end = chrono::steady_clock::now();
-	cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " мс"<< "\t";	//выводим значения в миллисекундах
-} 
-void print_head(vector<double>& vec)			//однократное выполнение и вывод подсчета времени потоков
-{	
-	//cout << "\t" << vec.size();
-	timeLaps(vec);
 
-}
-void print(vector<double>& vec, once_flag& o_flag)		//вывод на экран всех подсчетов
-{
-	call_once(o_flag, [&vec]() { print_head(vec); });	
-	//timeLaps(vec);
-}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int num_Vectors = 4;								//кол-во векторов
 	cout << "Количество аппаратных ядер - " << thread::hardware_concurrency() << "\n" << "\n\t\t";	//кол-во ядер
 
-	vector<vector<double>> vectors(num_Vectors);		//создаем вектор векторов (4 вектора с увеличивающимся кол-вом элементов
+	//создаем вектор векторов (4 вектора с увеличивающимся кол-вом элементов и результирующий
+	vector<vector<double>> vectors(num_Vectors);
+	vector<vector<double>> result(num_Vectors);
+
 	size_t num = 1000;									//начальная величина массива элементов для первого вектора
 	for (size_t x = 0; x < vectors.size(); x++)
 	{
 		vectors[x] = intialize(vectors[x], num);		//инициализируем все элементы векторов числами
+		result[x].resize(vectors[x].size());			//определяем сразу размер суммарного вектора
 		num *= 10;
 	}
 	cout << endl;		
 		
-	//Начинаем циклы подсчетов и выподим в виде таблицы
+	//Начинаем циклы подсчетов и выводим в виде таблицы
 
-		for (int num_threads = 1; num_threads <= 16; num_threads *= 2)	// кол-во потоков двукратно увеличиваем каждый раз
-		{		
-			cout << num_threads << " потоков\t";			// выводим на экран текущее кол-во потоков
-			vector<once_flag> o_flags(vectors.size());		// для каждого вектора свой флаг
-			vector<thread> vt;								//вектор потоков
-			for (size_t i = 0; i < num_threads; i++)		// для каждого потока
-			{				
-				for (size_t x = 0; x < vectors.size(); x++)	//считаем сумму каждого вектора и применяем флаг 
-				{
-					vt.emplace_back(thread(print, ref(vectors[x]), ref(o_flags[x])));
-				}		
-			}
-			for (auto& t : vt)				//ждем выполнения всех потоков
+	for (int num_threads = 1; num_threads <= 16; num_threads *= 2)	// кол-во потоков двукратно увеличиваем каждый раз
+	{
+		cout << num_threads << " потоков\t";			// выводим на экран текущее кол-во потоков
+		vector<thread> vt;								//вектор потоков
+		for (size_t x = 0; x < vectors.size(); x++)		//проходим для каждого вектора		
+		{	
+			auto start_time = chrono::steady_clock::now();	//начало отсчета времени на суммирование векторов
+
+			size_t part_of_vector = vectors[x].size() / num_threads;	//разбиваем каждый вектор на кол-во частей (диапазонов) равное кол-ву потоков
+			for (size_t i = 0; i < num_threads; i++)				//для каждого поотока свой диапазон одного вектора
 			{
-				t.join();
-			}			
-			vt.clear();				//очищаем потоки
-			cout << endl;
-		}	
-}
-
+				size_t start, end;
+				start = i * part_of_vector;			//указываем начало диапазона для потока
+				if (i == (num_threads - 1))	
+					end = vectors[x].size();		//конец диапазона если это последний поток
+				else 
+					end = start + part_of_vector;	//конец диапазона если поток не последний
+				
+				vt.emplace_back(thread(sumVector, cref(vectors[x]), cref(vectors[x]), ref(result[x]), start, end));
+			}
+			auto end_time = chrono::steady_clock::now();	//окончание отсчета времени для суммирования векторов
+			cout << chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() << " мкс" << "\t\t";	//выводим значения в микросекундах
+		}
+		for (auto& t : vt)				//ждем выполнения всех потоков
+		{
+			t.join();
+		}
+		vt.clear();				//очищаем потоки
+		cout << endl;
+	}			
+}	
