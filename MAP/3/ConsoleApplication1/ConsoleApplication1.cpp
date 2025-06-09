5f3c92c2830a83425ee3bcdb1992d5e6ddea1eee
myatezhnikov <myatezhnikov2014@gmail.com>
2025-06-09 21:07:22 +0400

    MAP_3

---
+++ MAP/3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm> 
+#include <random>   
+using namespace std;
+
+size_t find_minimum_element(const std::vector<int>& arr, size_t start, size_t end) // поиск индекса мин элемента от start до end
+{
+    size_t min_elem = start;
+    for (size_t i = start + 1; i < end; ++i) {
+        if (arr[i] < arr[min_elem]) {
+            min_elem = i;
+        }
+    }
+    return min_elem;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+   
+    const size_t size = 10;
+    std::vector<int> arr(size);
+
+    srand(time(0)); 
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; i++)              // Инициализация массива случайными числами
+    {
+        arr[i] = (rand() % 5 + 1) + (rand() % 100 + 10);
+        cout << arr[i] << " ";
+    }
+    cout << std::endl;
+  
+    for (size_t i = 0; i < arr.size(); ++i) 
+    {       
+        promise<size_t> prom;                       // promise и future для передачи результата поиска минимума
+        future<size_t> fut = prom.get_future();
+      
+        auto search_task = std::async(std::launch::async, [&arr, i, &prom]()      // асинхронная задача поиска минимума
+            {
+            size_t min_idx = find_minimum_element(arr, i, arr.size());
+            prom.set_value(min_idx);
+            });
+
+       
+        size_t min_elem = fut.get();      // индекс минимального элемента будущего
+       
+        if (min_elem != i)               // Меняем местами текущий элемент с минимальным
+        {
+            std::swap(arr[i], arr[min_elem]);
+        }
+    }
+   
+    cout << "Отсортированный массив: ";         // Вывод на экран после соритировки
+    for (const auto& num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ MAP/3/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+#include <iterator> 
+using  namespace std;
+
+const size_t threshold = 1000;                  // Пороговое значение для разделения
+template<typename Iterator, typename Func>
+
+void parallel_for_each(Iterator begin, Iterator end, Func func)     // функция параллельного for_each
+{
+    size_t distance = std::distance(begin, end);
+    if (distance <= threshold)                            // Если диапазон меньше порога (1000) то, обрабатываем обычно
+    {       
+        for_each(begin, end, func);  
+    }
+    else 
+    {       
+        Iterator mid = begin;                // иначе делим диапазон пополам
+        advance(mid, distance / 2);
+       
+        auto handle1 = std::async(std::launch::async, parallel_for_each<Iterator, Func>, begin, mid, func);  // две асинхронные задачи для обеих половин
+        auto handle2 = std::async(std::launch::async, parallel_for_each<Iterator, Func>, mid, end, func);   // для обеих половин
+              
+        handle1.get();
+        handle2.get();
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+   
+    std::vector<int> data(15);   
+    cout << "Исходные элементы: "; 
+    for (int i = 0; i < data.size(); ++i)                    // Создаем вектор элементов
+    {
+        data[i] = i;  
+        cout << data[i] << " ";
+    }
+    cout << endl;
+
+    auto print_element = [](int& x) { x *= 2;  };                // выводим каждый элемент и умножаем на 2             
+       
+    parallel_for_each(data.begin(), data.end(), print_element);      // параллельный for_each
+       
+    cout << "Элементы после умножения: ";             // Проверяем первые 25 элементов
+    for (size_t i = 0; i < data.size(); ++i) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+}
