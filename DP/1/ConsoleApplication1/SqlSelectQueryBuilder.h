d3990aaf5eb60bf29502742276e28d24b717ccfe
myatezhnikov <myatezhnikov2014@gmail.com>
2025-06-30 19:20:48 +0400

    DP_1

---
+++ DP/1/ConsoleApplication1/SqlSelectQueryBuilder.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+class SqlSelectQueryBuilder {
+public:
+  
+    SqlSelectQueryBuilder& AddColumn(const string& column)           // Добавление колонки
+    {
+        columns_.push_back(column);
+        return *this;
+    }
+   
+    SqlSelectQueryBuilder& AddFrom(const string& table_name)        // Установка таблицы 
+    {
+        table_ = table_name;
+        return *this;
+    }
+   
+    SqlSelectQueryBuilder& AddWhere(const string& column, const string& value)     // Добавление условия WHERE
+    {
+        where_conditions_.push_back({ column, value });
+        return *this;
+    }
+
+   
+    string BuildQuery() const                    // Запрос
+    {
+        ostringstream query;       
+        query << "SELECT ";                          // Начало строки
+
+        if (columns_.empty()) 
+        {
+            query << "*";
+        }
+        else {
+            for (size_t i = 0; i < columns_.size(); ++i) 
+            {
+                query << columns_[i];
+                if (i != columns_.size() - 1) 
+                {
+                    query << ", ";
+                }
+            }
+        }
+              
+        query << " FROM " << table_;            // From
+               
+        if (!where_conditions_.empty())         //Where
+        {
+            query << " WHERE ";
+            for (size_t i = 0; i < where_conditions_.size(); ++i) 
+            {
+                const auto& cond = where_conditions_[i];
+                query << cond.first << "=" << cond.second;
+                if (i != where_conditions_.size() - 1)
+                {
+                    query << " AND ";
+                }
+            }
+        }
+       
+        query << ";";                   //конец строки
+        return query.str();
+    }
+
+private:
+    vector<string> columns_;
+    string table_ = "default_table";       // По умолчанию
+    vector<pair<string, string>> where_conditions_;
+};
---
+++ DP/1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cassert>
+#include "SqlSelectQueryBuilder.h"
+using namespace std;
+
+int main()
+{
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere({ {"id", "42"}, {"name", "John"} });
+
+    assert(query_builder.BuildQuery() == "SELECT name, phone FROM students WHERE id=42 AND name=John;");    //Проверяем
+
+    cout << query_builder.BuildQuery() << endl;       //Вывод на экран
+
+}
