001d2e3df187c3ea263da9b0ed7603a3ad94dfcb
myatezhnikov <myatezhnikov2014@gmail.com>
2025-07-03 20:41:53 +0400

    DP_2

---
+++ DP/2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,110 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+class Text 
+{
+    public:
+        virtual void render(const std::string& data) const 
+        {
+            cout << data;
+        }
+};
+
+class DecoratedText : public Text 
+{
+    public:
+         explicit DecoratedText(Text* text) : text_(text) {}
+         Text* text_;
+};
+
+class ItalicText : public DecoratedText 
+{
+    public:
+         explicit ItalicText(Text* text) : DecoratedText(text) {}
+         void render(const std::string& data)  const 
+         {
+              cout << "<i>";
+              text_->render(data);
+              cout << "</i>";
+         }
+};
+
+class BoldText : public DecoratedText 
+{
+    public:
+        explicit BoldText(Text* text) : DecoratedText(text) {}
+        void render(const std::string& data) const 
+        {
+            cout << "<b>";
+            text_->render(data);
+            cout << "</b>";
+        }
+};
+
+class Paragraph : public Text       // класс параграф
+{
+public:
+    explicit Paragraph(Text* text) : text_(text) {}   
+
+    void render(const std::string& data) const override {
+        std::cout << "<p>";
+        text_->render(data);
+        cout << "</p>" << endl;
+    }
+private:
+    Text* text_;
+};
+
+class Reversed : public Text                 // класс разворота строки
+{
+public:
+    explicit Reversed(Text* text) : text_(text) {}   
+    void render(const std::string& data) const override 
+    {
+        string reversed_data = data;
+        reverse(reversed_data.begin(), reversed_data.end());
+        text_->render(reversed_data);
+        cout << endl;
+    }   
+private:
+    Text* text_;
+};
+
+class Link : public Text            //класс Link с другой сигнатурой
+{
+public:
+    explicit Link(Text* text) : text_(text) {}  
+   
+    void render(const std::string& url, const std::string& data) const   // другой метод
+    {
+        cout << "<a href=" << url << ">";
+        text_->render(data);
+        cout << "</a>" << endl;
+    }
+   
+    void render(const std::string& data) const override          // реализация метода
+    {
+        text_->render(data);
+    }
+private:
+    Text* text_;
+};
+
+int main() 
+{
+    //проверяем
+    auto text_block = new Paragraph(new Text());
+    text_block->render("Hello world");
+    delete text_block;
+
+    auto text_block1 = new Reversed(new Text());
+    text_block1->render("Hello world");
+    delete text_block1;
+       
+    auto text_block2 = new Link(new Text());
+    text_block2->render("netology.ru", "Hello world");
+    delete text_block2;
+  
+}
---
+++ DP/2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,83 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class VeryHeavyDatabase {
+public:
+   virtual string GetData(const string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class OneShotDB : public VeryHeavyDatabase          //дополнительный класс с лимитом получения данных
+{
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object_(real_object), remaining_shots_(shots) { }
+
+    string GetData(const string& key) const noexcept override 
+    {
+        if (remaining_shots_ == 0) 
+        {
+            return "error";
+        }
+        --remaining_shots_;
+        return real_object_->GetData(key);
+    }
+
+private:
+    VeryHeavyDatabase* real_object_;
+    mutable size_t remaining_shots_; // mutable, чтобы можно было изменять в const методе
+};
+
+class CacheProxyDB : public VeryHeavyDatabase 
+{
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    string GetData(const string& key) noexcept 
+    {
+        if (cache_.find(key) == cache_.end()) 
+        {
+            cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else 
+        {
+            cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    map<string, string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase 
+{
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    string GetData(const string& key) noexcept 
+    {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+
+int main() {
+  /*  auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    cout << cached_db.GetData("key") << endl;
+    cout << cached_db.GetData("key") << endl;
+    cout << test_db.GetData("key") << endl;
+   */
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(addressof(real_db), 2);
+
+    cout << limit_db.GetData("key") << endl; 
+    cout << limit_db.GetData("key") << endl; 
+    cout << limit_db.GetData("key") << endl; 
+}
